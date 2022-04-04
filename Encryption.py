dc902dfb8bf3449722db7deae9a1d508b66a0144
Shrirang Mahajan <85283622+NotShrirang@users.noreply.github.com>
2022-04-04 19:53:28 +0530

    Add files via upload

---
+++ Encryption.py
@@ -0,0 +1,75 @@
+import random
+
+def Encrypt(x):
+    m=len(list(x))
+    i=0
+    y=str()
+    mylist = []
+    answerKey = []
+    while i<m:
+        ch=x[i]
+        ch = bytes(ch, 'utf-8')
+        r=random.randint(0,5)
+        s=bytes([ch[0] + r])
+        s=str(s)
+        y=y+s[2]
+        i=i+1
+        answerKey.append(str(r))
+    return y, answerKey
+def encryptWithKey(x, answerKey):
+    # m=len(list(x))
+    i=0
+    x = list(x)
+    y=str()
+    for a in answerKey:
+        ch=x[i]
+        ch = bytes(ch, 'utf-8')
+        s=bytes([ch[0] + int(a)])
+        s=str(s)
+        y=y+s[2]
+        i=i+1
+    return y
+def Decrypt(msg, answerKey):
+    i=0
+    x=str()
+    a=list()
+    for j in answerKey:
+        num = j
+        num = int(num)
+        a.append(num)
+        ch = msg[i]
+        ch = bytes(ch,'utf-8')
+        s = bytes([ch[0] - num])
+        s = str(s)
+        x = x+s[2]
+        i = i+1
+    return x
+def decryptorFormatting(key):
+    key = key.replace("[", "")
+    key = key.replace("'", "")
+    key = key.replace(",", "")
+    key = key.replace(" ", "")
+    key = key.replace("]", "")
+    return list(key)
+def hash(msg):
+    l = len(list(msg))
+    s = ""
+    y = str()
+    for i in range(l):
+        ch = msg[i]
+        ch = bytes(ch, 'utf-8')
+        ch = bytes([ch[0]+i])
+        s = str(ch)
+        y = y + str(s[2])
+    return y
+def deHash(msg):
+    l = len(list(msg))
+    s = ""
+    y = str()
+    for i in range(l):
+        ch = msg[i]
+        ch = bytes(ch, 'utf-8')
+        ch = bytes([ch[0]-i])
+        s = str(ch)
+        y = y + str(s[2])
+    return y
