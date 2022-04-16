da4b4e0fa7c0cf28519d93f8d97cea1b36b81335
Shrirang Mahajan <85283622+NotShrirang@users.noreply.github.com>
2022-04-16 09:19:36 +0530

    Update Encryption.py

--- Encryption.py
+++ Encryption.py
@@ -1,9 +1,17 @@
 import random
 
-def Encrypt(x):
+def Encrypt(msg:str|list):
+    '''
+    Function for encrypting text.
+    
+    args :
+        msg (str) : Text to be encrypted.
+    Returns : 
+        Tuple of encrypted message and answerkey for decryption. -> tuple[str, list]
+    '''
     y=str()
     answerKey = []
-    for ch in x:
+    for ch in msg:
         ch = bytes(ch, 'utf-8')
         r=random.randint(0,5)
         s=bytes([ch[0] + r])
@@ -11,42 +19,73 @@ def Encrypt(x):
         y=y+s[2]
         answerKey.append(str(r))
     return y, answerKey
-def encryptWithKey(x, answerKey):
-    # m=len(list(x))
+def encryptWithKey(msg : str, key : list):
+    '''
+    Function for encrypting text with user provided key.
+    
+    args :
+        msg (str) : Text to be encrypted.
+        key (list) : Key for encryption.
+    Returns : 
+        Encrypted message in string format.
+    '''
     i=0
-    x = list(x)
+    msg = list(msg)
     y=str()
-    for a in answerKey:
-        ch=x[i]
+    for a in key:
+        ch=msg[i]
         ch = bytes(ch, 'utf-8')
         s=bytes([ch[0] + int(a)])
         s=str(s)
         y=y+s[2]
         i=i+1
     return y
-def Decrypt(msg, answerKey):
+def Decrypt(msg : str, key: list):
+    '''
+    Function for decrypting text.
+    
+    args :
+        msg (str) : Encrypted text.
+        key (list) : Key for decryption.
+    Returns : 
+        Decrypted message in string format.
+    '''
     i=0
     x=str()
     a=list()
-    for j in answerKey:
-        num = j
-        num = int(num)
-        a.append(num)
+    for j in key:
+        a.append(int(j))
         ch = msg[i]
         ch = bytes(ch,'utf-8')
-        s = bytes([ch[0] - num])
+        s = bytes([ch[0] - int(j)])
         s = str(s)
         x = x+s[2]
         i = i+1
     return x
-def decryptorFormatting(key):
+def decryptorFormatting(key : str):
+    '''
+    Function for formatting given str(key) to list(key) in order to pass it to Decrypt().
+    
+    args :
+        key (str) : key for decryption.
+    Returns : 
+        Converted key in list format.
+    '''
     key = key.replace("[", "")
     key = key.replace("'", "")
     key = key.replace(",", "")
     key = key.replace(" ", "")
     key = key.replace("]", "")
     return list(key)
-def hash(msg):
+def hash(msg : str):
+    '''
+    Function for hashing the message passed.
+
+    args :
+        msg (str) : Text to be hashed.
+    Returns : 
+        hashed message.
+    '''
     l = len(list(msg))
     s = ""
     y = str()
@@ -54,16 +93,6 @@ def hash(msg):
         ch = msg[i]
         ch = bytes(ch, 'utf-8')
         ch = bytes([ch[0]+i])
-        s = str(ch)
-        y = y + str(s[2])
-def deHash(msg):
-    l = len(list(msg))
-    s = ""
-    y = str()
-    for i in range(l):
-        ch = msg[i]
-        ch = bytes(ch, 'utf-8')
-        ch = bytes([ch[0]-i])
         s = str(ch)
         y = y + str(s[2])
     return y
